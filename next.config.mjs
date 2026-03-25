9eaefc46e4f8a0d0a86d3d83b6e7134fc783913f
WaranonSop <pipezaza34r@gmail.com>
2026-03-25 23:32:41 +0700

    auth + admin-panel

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false, // เพิ่มบรรทัดนี้เพื่อแก้ Error Swagger
   allowedDevOrigins: ["192.168.1.34"],
-  /* config options here */
   reactCompiler: true,
-  transpilePackages: ['swagger-ui-react'],
+  transpilePackages: ["swagger-ui-react"],
 };
 
 export default nextConfig;
